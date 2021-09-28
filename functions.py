2ff7e5a5150530ecf9cf2cdd05ecf6bb4ddbe870
Igor M <eyegor.m@gmail.com>
2021-09-28 13:27:41 +0300

    upd

--- functions.py
+++ functions.py
@@ -11,16 +11,45 @@ from config import (
 
 ##### libraries
 from requests import Request, Session
-
+from bs4 import BeautifulSoup
 
 
 
 ###### functions
-def get_projects_dict():
+def get_projects():
+    projects = dict()
     with Session() as s:
         s.auth = (OLD_SQUASH_USER,OLD_SQUASH_PASS)
-        resp = s.get(OLD_SQUASH_URL + '/administration/projects').content.decode()
-    return resp
+        raw_resp = s.get(OLD_SQUASH_URL + '/administration/projects').content.decode()
+    
+    parsed_resp = BeautifulSoup(raw_resp, "lxml")
+    for row in parsed_resp.find_all('tr'):
+        pr_id = (row.find('td', attrs={'class':'project-id'}))
+        pr_name = (row.find('td', attrs={'class':'name'}))
+        if pr_id and pr_name != None:
+            projects[pr_id.text] = pr_name.text
+  
+    return projects
+
+def get_requirements():
+    pass
+
+def get_test_cases():
+    test_cases = dict()
+    with Session() as s:
+        s.auth = (OLD_SQUASH_USER,OLD_SQUASH_PASS)
+        raw_resp = s.get(OLD_SQUASH_URL + '/test-case-workspace').content.decode()
+    
+    parsed_resp = BeautifulSoup(raw_resp, "lxml")
+    #for row in parsed_resp.find_all('tr'):
+    #    pr_id = (row.find('td', attrs={'class':'project-id'}))
+    #    pr_name = (row.find('td', attrs={'class':'name'}))
+    #    if pr_id and pr_name != None:
+    #        projects[pr_id.text] = pr_name.text
+
+
+  
+    return parsed_resp
 
 
 
--- start.py
+++ start.py
@@ -9,7 +9,9 @@ from functions import *
 
 ###### start 
 def main():
-    resp = get_projects_dict()
+    #resp = get_projects_dict()
+    resp = get_test_cases_dict()
+
     #print(type(resp))
     print(resp)
  
